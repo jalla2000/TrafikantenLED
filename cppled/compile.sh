719858d11347157aa23a45e0557ce0caf4205a38
Pål Driveklepp <paal.driveklepp@bluectrl.io>
2024-09-22 22:16:26 +0200

    Add CMakeLists.txt

--- cppled/compile.sh
+++ cppled/compile.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 # Remember to install libcurlpp-dev libjsoncpp-dev
 set -x
+# Old: (use cmake instead)
 g++ -g -Wall -Wextra -Werror -std=c++17 sprite.cpp leddisplay.cpp http_fetchers.cpp command_line.cpp main.cpp -lcurl -ljsoncpp -lncurses
+
+# To build:
+# mkdir build
+# cd build
+# cmake ..
+# cmake --build .
